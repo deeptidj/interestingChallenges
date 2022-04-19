7cf95d5e0095891ca29f407bf3b387d28230cb16
Deepti Jain <deeptidj@gmail.com>
2022-04-18 17:13:47 -0700

    Updates to existing programs

---
+++ src/main/java/com/facebook/challenges/ConvertBinaryTreeDoublyLinkedList.java
@@ -0,0 +1,14 @@
+package com.facebook.challenges;
+
+public class ConvertBinaryTreeDoublyLinkedList {
+    public static void main(String[] args) {
+        System.out.println("joe mama is susy baka");
+        System.out.println("please keep this code in and not delete the joe mama part   preety please!! with a cherry on top or a strawbery or a rasberry or whatever you like.!!");
+        System.out.println("Process finished with exit code 21");
+        System.out.println("Process finished with exit code 21");
+        System.out.println("Process finished with exit code 21");
+        System.out.println("Process finished with exit code 21");
+    }
+
+
+}
--- src/main/java/com/interviewCamp/problems/DutchNationalFLag.java
+++ src/main/java/com/interviewCamp/problems/DutchNationalFLag.java
@@ -4,6 +4,12 @@ public class DutchNationalFLag {
     public static void main(String[] args) {
         int arr[] = {1,6,2,5,3,4,4,4,5,7,8,7,4};
 
+
+        int[] res = dutchNationalFlag(arr, 4);
+        for (int i = 0; i < res.length; i++) {
+            System.out.print(res[i]+ " ");
+        }
+        System.out.println("******");
         int i = 0; int low = 0; int hi = arr.length -1;
         int pivot = 4;
         //loop the i until the hi and not the array length since we want to dont want to go over the hi bubble that has been already sorted out.
@@ -31,5 +37,27 @@ public class DutchNationalFLag {
         arr[y] = tmp;
         return arr;
     }
+    public static void printArray(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static int[] dutchNationalFlag(int[] arr, int target) {
+        printArray(arr);
+        int low=0; int hi=arr.length-1; int i = 0;
+        while(i < hi){
+            if(arr[i] < target) {
+                swap(arr, i,low);
+                low++;i++;
+            } else if (arr[i] > target) {
+                swap(arr, i, hi);
+                hi--;
+            } else {
+                i++;
+            }
+        }
+        return arr;
 
+    }
 }
